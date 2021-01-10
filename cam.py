f55b239acde17becfa3cc8a442cd4c267c73cb52
SH1SH4 <dshisha0@gmail.com>
2021-01-10 21:15:16 +0300

    enemy

--- cam.py
+++ cam.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 import sys
+from random import randint
 import pytmx
 import pyscroll
 from pytmx.util_pygame import load_pygame
@@ -18,7 +19,7 @@ screen_size = (1920, 1020)
 map_layer = pyscroll.BufferedRenderer(map_data, screen_size, True)
 group = pyscroll.PyscrollGroup(map_layer=map_layer)
 obstacles = pygame.sprite.Group()
-hero = pygame.sprite.Group()
+enemy = pygame.sprite.Group()
 
 
 class Map:
@@ -29,11 +30,37 @@ class Map:
         self.tile_size = self.map.tilewidth
         self.free_tile = free_tile
 
+    def find_path(self, start, target):
+        INF = 1000
+        x, y = start
+        distance = [[INF] * self.width for _ in range(self.height)]
+        distance[y][x] = 0
+        prev = [[None] * self.width for _ in range(self.height)]
+        queue = [(x, y)]
+        while queue:
+            x. y = queue.pop(0)
+            for dx, dy in (1, 0), (0, 1), (-1, 0), (0, -1):
+                next_x, next_y = x + dx, x + dy
+                if 0 <= next_x < self.width and 0 < next_y < self.height and self.is_free((next_x, next_y)) and distance[next_y][next_x] == INF:
+                    distance[next_y][next_x] = distance[y][x] + 1
+                    prev[next_y][next_x] = (x, y)
+                    queue.append(next_x, next_y)
+        x, y = target
+        if distance[y][x] == INF or start == target:
+            return start
+        while prev[y][x] != start:
+            x, y = prev[y][x]
+        return x, y
+
     def render(self):
         for y in range(self.height):
             for x in range(self.width):
                 if self.map.tiledgidmap[self.map.get_tile_gid(x, y, 0)] not in self.free_tile:
                     Obstacles(self.map.get_tile_image(x, y, 0), x * self.tile_size, y * self.tile_size)
+        for i in range(100):
+            x, y = (randint(0, self.width - 1), randint(0, self.height - 1))
+            if self.map.tiledgidmap[self.map.get_tile_gid(x, y, 0)] in self.free_tile:
+                Enemy((x * self.tile_size, y * self.tile_size), load_image("llama (1).png"), 3, 2)
 
     def get_tile_id(self, position):
         return self.map.tiledgidmap[self.map.get_tile_gid(*position, 0)]
@@ -81,14 +108,17 @@ class Button:
 class Hero(pygame.sprite.Sprite):
     def __init__(self, position, sheet, columns, rows, x, y):
         pygame.sprite.Sprite.__init__(self, group)
-
         self.frames = []
         self.cut_sheet(sheet, columns, rows)
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
         self.rect.x, self.rect.y = position
         self.delay = 0
+        self.speed = 20
         self.add(hero)
+
+    def get_position(self):
+        return self.rect.x, self.rect.y
 
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns, sheet.get_height() // rows)
@@ -106,25 +136,51 @@ class Hero(pygame.sprite.Sprite):
     def update(self, world, delta_time):
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
-            self.rect.x -= 5
+            self.rect.x -= self.speed
             self.animation()
             if pygame.sprite.spritecollideany(self, obstacles):
-                self.rect.x += 5
+                self.rect.x += self.speed
         if key[pygame.K_RIGHT]:
-            self.rect.x += 5
+            self.rect.x += self.speed
             self.animation()
             if pygame.sprite.spritecollideany(self, obstacles):
-                self.rect.x -= 5
+                self.rect.x -= self.speed
         if key[pygame.K_UP]:
-            self.rect.y -= 5
+            self.rect.y -= self.speed
             self.animation()
             if pygame.sprite.spritecollideany(self, obstacles):
-                self.rect.y += 5
+                self.rect.y += self.speed
         if key[pygame.K_DOWN]:
-            self.rect.y += 5
+            self.rect.y += self.speed
             self.animation()
             if pygame.sprite.spritecollideany(self, obstacles):
-                self.rect.y -= 5
+                self.rect.y -= self.speed
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, position, sheet, columns, rows):
+        pygame.sprite.Sprite.__init__(self, group)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect.x, self.rect.y = position
+        self.delay = 0
+        self.add(enemy)
+
+    def get_position(self):
+        return self.rect.x, self.rect.y
+
+    def update(self, world, delta_time):
+        next_position = world.find_path((self.rect.x, self.rect.y), hero.get_position)
+        self.rect.x, self.rect.y = next_position
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns, sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(frame_location, self.rect.size)))
 
 
 def load_image(name, colorkey=None):
@@ -172,10 +228,8 @@ def start_game():
     running = True
     screen.fill((0, 0, 0))
     world = Map("poligon2.0.tmx", [30, 15])
-    hero = Hero((50, 50), load_image("llama (1).png"), 3, 2, 50, 50)
-    # game = Game(world, hero)
     world.render()
-    clock = pygame.time.Clock()
+    hero = Hero((50, 50))
     fps = 60
     clock = pygame.time.Clock()
     while running:
@@ -188,7 +242,7 @@ def start_game():
         group.center(hero.rect.center)
         group.draw(screen)
         pygame.display.flip()
-
     pygame.quit()
+
 
 start_screen()
